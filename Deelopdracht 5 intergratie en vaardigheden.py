be1c62b54d790b8a96ab775d51e3d88662a7e2bc
DS3008 <dfstam@tudelft.nl>
2024-12-09 12:25:06 +0100

    Update Deelopdracht 5 intergratie en vaardigheden.py

--- Deelopdracht 5 intergratie en vaardigheden.py
+++ Deelopdracht 5 intergratie en vaardigheden.py
@@ -5,3 +5,53 @@ Created on Mon Dec  9 12:21:42 2024
 @author: daphn
 """
 
+#GEWICHTEN
+m_windmolendeel = 400000 #kg
+SWLmax = ((m_windmolendeel/94)*100) #kg
+
+m_kraanhuis = (0.34*SWLmax) #kg
+m_kraanboom = (0.17*SWLmax) #kg
+m_hijsgerei = (0.06*SWLmax) #kg
+m_kraan = (m_kraanboom + m_kraanhuis + m_hijsgerei + m_windmolendeel) #kg, inclusief windmolendeel
+m_scheepslading = 4 * 230000
+
+#ZWAARTEPUNTEN
+ZPx_windmolendeel = 24.25 #zwaartpunt van het hangende windmolendeel op de x-as
+ZPy_windmolendeel = 32 #zwaartepunt van het hangende windmolendeel op de y-as
+ZPx_kraanhuis = 8 #zwaartepunt van het kraanhuis op de x-as
+ZPy_kraanhuis = 32 #zwaartepunt van het kraanhuis op de y-as
+ZPx_kraanboom = 16.125 #zwaartepunt van de kraanboom op de x-as
+ZPy_kraanboom = 32 #zwaartepunt van de kraanboom op de y-as
+ZPx_hijsgerei = 24.25 #zwaartepunt van het hijsgerei op de x-as
+ZPy_hijsgerei = 32 #zwaartepunt van het hijsgerei op de y-as
+ZPx_kraan = ((m_kraanhuis * ZPx_kraanhuis) + (m_kraanboom * ZPx_kraanboom) + (m_hijsgerei * ZPx_hijsgerei) + (ZPx_windmolendeel * m_windmolendeel)) / m_kraan #zwaartepunt van de kraan op de x-as, inclusief windmolendeel
+ZPy_kraan = 32 #zwaartepunt van de kraan op de y-as
+ZPx_boot = 10 #zwaartpunt van de boot op de x-as
+ZPy_boot = 35 #zwaartepunt van de boot op de y-as
+
+#AFMETINGEN
+l_kraanboom = 32.5 #meter, lengte kraanboom
+l_boot = 70 #meter, lengte boot
+b_boot = 20 #meter, breedte boot
+h_boot = 13 #meter, diepte boot
+d_boot = 10 #meter, diepgang boot
+ll_ballasttank = 70 #meter, lengte lange ballasttank
+bl_ballasttank = 6.65 #meter, breedte lange ballasttank
+dl_ballasttank = 13 #meter, diepte lange ballasttank
+lk_ballasttank = 34 #meter, lengte korte ballasttank
+bk_ballasttank = 6.70 #meter, breedte korte ballasttank
+dk_ballasttank = 13 #meter, diepte korte ballasttank
+
+diameter_windmolendeel = 8 #m
+
+pd_zijwanden = 0.02 #meter, plaatdikte zijwanden
+pd_tankwanden = 0.01 #meter, plaatdikte tankwanden
+pd_schot = 0.01 #meter, plaatdikte voor/achterschip
+
+#OVERIGE GEGEVENS
+soortelijk_staalgewicht = 7850 #kg/m3
+soortelijk_zeewater = 1025 #kg/m3
+f_verstijfers = 2.1 #factor 2.1 keer het totale gewicht
+gv = 9.81 #m/s, gravitatieversnelling
+app = 4.2 #meter
+vg_tank3 = 0.72 #vullingsgraad tank 3
