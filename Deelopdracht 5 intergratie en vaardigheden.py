cc9c2ef916c07f8169d5e0369175fe92cb09ff2d
DS3008 <dfstam@tudelft.nl>
2024-12-09 14:26:11 +0100

    Update Deelopdracht 5 intergratie en vaardigheden.py
    Afmetingen boven de zwaartepunten gezet

--- Deelopdracht 5 intergratie en vaardigheden.py
+++ Deelopdracht 5 intergratie en vaardigheden.py
@@ -7,28 +7,14 @@ Created on Mon Dec  9 12:21:42 2024
 import numpy as np
 
 #GEWICHTEN
-m_windmolendeel = 400000 #kg
+m_windmolendeel = 230000 #kg
 SWLmax = ((m_windmolendeel/94)*100) #kg
 
 m_kraanhuis = (0.34*SWLmax) #kg
 m_kraanboom = (0.17*SWLmax) #kg
 m_hijsgerei = (0.06*SWLmax) #kg
 m_kraan = (m_kraanboom + m_kraanhuis + m_hijsgerei + m_windmolendeel) #kg, inclusief windmolendeel
-m_scheepslading = 4 * 230000
-
-#ZWAARTEPUNTEN
-ZPx_windmolendeel = l_kraanboom*np.cos(60) #zwaartpunt van het hangende windmolendeel op de x-as
-ZPy_windmolendeel = l_boot/2 #zwaartepunt van het hangende windmolendeel op de y-as
-ZPx_kraanhuis = 0 #zwaartepunt van het kraanhuis op de x-as
-ZPy_kraanhuis = l_boot/2 #zwaartepunt van het kraanhuis op de y-as
-ZPx_kraanboom = l_kraanboom*np.cos(60)/2 #zwaartepunt van de kraanboom op de x-as
-ZPy_kraanboom = l_boot/2 #zwaartepunt van de kraanboom op de y-as
-ZPx_hijsgerei = ZPx_windmolendeel #zwaartepunt van het hijsgerei op de x-as
-ZPy_hijsgerei = l_boot/2 #zwaartepunt van het hijsgerei op de y-as
-ZPx_kraan = ((m_kraanhuis * ZPx_kraanhuis) + (m_kraanboom * ZPx_kraanboom) + (m_hijsgerei * ZPx_hijsgerei) + (ZPx_windmolendeel * m_windmolendeel)) / m_kraan #zwaartepunt van de kraan op de x-as, inclusief windmolendeel
-ZPy_kraan = l_boot/2 #zwaartepunt van de kraan op de y-as
-ZPx_boot = 10 #zwaartpunt van de boot op de x-as
-ZPy_boot = 35 #zwaartepunt van de boot op de y-as
+m_scheepslading = 4 * m_windmolendeel
 
 #AFMETINGEN
 l_kraanboom = 32.5 #meter, lengte kraanboom
@@ -48,6 +34,20 @@ diameter_windmolendeel = 8 #m
 pd_zijwanden = 0.02 #meter, plaatdikte zijwanden
 pd_tankwanden = 0.01 #meter, plaatdikte tankwanden
 pd_schot = 0.01 #meter, plaatdikte voor/achterschip
+
+#ZWAARTEPUNTEN
+ZPx_windmolendeel = l_kraanboom * np.cos(60) #zwaartpunt van het hangende windmolendeel op de x-as
+ZPy_windmolendeel = l_boot / 2 #zwaartepunt van het hangende windmolendeel op de y-as
+ZPx_kraanhuis = 0 #zwaartepunt van het kraanhuis op de x-as
+ZPy_kraanhuis = l_boot/2 #zwaartepunt van het kraanhuis op de y-as
+ZPx_kraanboom = l_kraanboom * np.cos(60) / 2 #zwaartepunt van de kraanboom op de x-as
+ZPy_kraanboom = l_boot / 2 #zwaartepunt van de kraanboom op de y-as
+ZPx_hijsgerei = ZPx_windmolendeel #zwaartepunt van het hijsgerei op de x-as
+ZPy_hijsgerei = l_boot / 2 #zwaartepunt van het hijsgerei op de y-as
+ZPx_kraan = ((m_kraanhuis * ZPx_kraanhuis) + (m_kraanboom * ZPx_kraanboom) + (m_hijsgerei * ZPx_hijsgerei) + (ZPx_windmolendeel * m_windmolendeel)) / m_kraan #zwaartepunt van de kraan op de x-as, inclusief windmolendeel
+ZPy_kraan = l_boot / 2 #zwaartepunt van de kraan op de y-as
+ZPx_boot = 10 #zwaartpunt van de boot op de x-as
+ZPy_boot = 35 #zwaartepunt van de boot op de y-as
 
 #OVERIGE GEGEVENS
 soortelijk_staalgewicht = 7850 #kg/m3
